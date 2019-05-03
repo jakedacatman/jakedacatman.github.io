9cf8f21191746836b5cb28721a49738bc0ae1740
jakedacatman <helloihaxxedu@gmail.com>
2019-05-03 18:16:48 -0400

    update packages

--- commit.sh
+++ commit.sh
@@ -1,12 +1,12 @@
-rm -rf ./Packages*
-echo "" >> ./Packages
+rm -rf Packages*
+echo "" >> Packages
 
 
-/var/mobile/jakedacatman.github.io/dpkg-scanpackages -m /dev/null/ > ./Packages;
+/var/mobile/jakedacatman.github.io/dpkg-scanpackages -m . /dev/null > Packages;
 echo "zipping"
-bzip2 -c9k ./Packages > ./Packages.bz2;
+bzip2 -c9k Packages > Packages.bz2;
 
-git add --all
+git add -a
 git commit -m $*
 git push
 exit 0;
