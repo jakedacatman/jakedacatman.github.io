aac21876fe84df72d4628578f85508813be41171
jakedacatman <helloihaxxedu@gmail.com>
2019-05-21 14:36:16 -0400

    ok

--- vanitykristios/jcRootViewController.h
+++
@@ -1,3 +0,0 @@
-@interface jcRootViewController : UITableViewController
-
-@end
--- vanitykristios/main.m
+++
@@ -1,7 +0,0 @@
-#import "jcAppDelegate.h"
-
-int main(int argc, char *argv[]) {
-	@autoreleasepool {
-		return UIApplicationMain(argc, argv, nil, NSStringFromClass(jcAppDelegate.class));
-	}
-}
